6b68f5d04218541f5f9b733ef4bc6d236acce48b
Sergio D. Barrera <sdbeng@gmail.com>
2020-10-14 16:52:29 -0700

    Add title template, install markdown2, render

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+import markdown2
 
 from . import util
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def wiki(request, title):
+    # title
+    return render(request, "encyclopedia/title.html", {
+        "title_content": markdown2.markdown(util.get_entry(title)), "title": title
     })
