5a38df454958ee1c60f0bb523569a9e03c789547
Sergio D. Barrera <sdbeng@gmail.com>
2020-10-15 09:12:06 -0700

    Fix entry does not exist feature for wiki/title

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import Http404, HttpResponse, HttpResponseNotFound
 import markdown2
+from markdown2 import Markdown
 
 from . import util
 
@@ -10,11 +11,16 @@ def index(request):
     })
 
 def wiki(request, title):
+    mk = Markdown()
     # ask if title exists
-    # print(f"LOG title: {title}")
-    if util.list_entries() == None:
+    print(f"LOG title: {title}")
+    print(f"LOG util.get_entry(title): {util.get_entry(title)}")
+    entry_title = util.get_entry(title)
+    if entry_title is None:
         return HttpResponseNotFound(f" The entry with name {title} was not found.")
-    return render(request, "encyclopedia/title.html", {
-        "title_content": markdown2.markdown(util.get_entry(title)), "title": title
-    })
+    else:
+        return render(request, "encyclopedia/title.html", {
+            "title_content": mk.convert(util.get_entry(title)),
+            "title": title
+        })
 
