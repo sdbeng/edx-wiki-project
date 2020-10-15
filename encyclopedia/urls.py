c60438a253e80b1c0d91aa3b2fe14cd88ebfd215
Sergio D. Barrera <sdbeng@gmail.com>
2020-10-15 08:09:42 -0700

    Update index to display contents of entry

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+# app_name = 'encyclopedia'
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.wiki, name="wiki"),
+    # path("<str:entry_id>", views.detail, name="detail")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404, HttpResponse, HttpResponseNotFound
 import markdown2
 
 from . import util
@@ -9,7 +10,11 @@ def index(request):
     })
 
 def wiki(request, title):
-    # title
+    # ask if title exists
+    # print(f"LOG title: {title}")
+    if util.list_entries() == None:
+        return HttpResponseNotFound(f" The entry with name {title} was not found.")
     return render(request, "encyclopedia/title.html", {
         "title_content": markdown2.markdown(util.get_entry(title)), "title": title
     })
+
