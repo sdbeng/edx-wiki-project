6c3b5d8a45f9178faeb7d04d0148208cbf5da462
Sergio D. Barrera <sdbeng@gmail.com>
2020-10-15 12:46:36 -0700

    New page feature form displayed still bugs

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,5 +7,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.wiki, name="wiki"),
-    # path("<str:entry_id>", views.detail, name="detail")
+    path("add", views.add, name="add"),
+    path("search", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-from django.http import Http404, HttpResponse, HttpResponseNotFound
+from django import forms
+from django.http import Http404, HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 import markdown2
 from markdown2 import Markdown
+from django.urls import reverse
 
 from . import util
+# ot use forms create class
+class NewTitleForm(forms.Form):
+    title=forms.CharField(label="New Title")
+    content=forms.Textarea()
 
 def index(request):
+    # print(util.list_entries())
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
@@ -14,7 +21,7 @@ def wiki(request, title):
     mk = Markdown()
     # ask if title exists
     print(f"LOG title: {title}")
-    print(f"LOG util.get_entry(title): {util.get_entry(title)}")
+    # print(f"LOG util.get_entry(title): {util.get_entry(title)}")
     entry_title = util.get_entry(title)
     if entry_title is None:
         return HttpResponseNotFound(f" The entry with name {title} was not found.")
@@ -24,3 +31,45 @@ def wiki(request, title):
             "title": title
         })
 
+def search(request):
+    querySearch = request.GET.get('q', '') # q is the str name in the url and in the input tag
+    print(f"query: {querySearch}")
+    entries_list = util.list_entries()
+    print(f"entries_list {entries_list}")
+    new_list = []
+    if querySearch is not None:
+        return HttpResponseRedirect("/")
+        # return HttpResponseRedirect(reverse("wiki"), kwargs={'title': querySearch})
+    else:
+        for entry in entries_list:
+            if querySearch in entry:
+                new_list.append(entry)
+            
+        return render(request, "encyclopedia/index.html", {
+            "new_list": new_list,
+            "search": True,
+            "querySearch": querySearch
+        })
+
+def add(request):
+    if request.method == "POST":
+        # save new data entry in the form var
+        form = NewTitleForm(request.POST)
+        # then do server-side validation
+        if form.is_valid:
+            # if title is valid, save it
+            # Note1: ["title"] is the class title field prop
+            # Note2: ["content"] is the class content field prop
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+            # then add the title and content to the list
+            # todo: save_entry(title, content)
+        else:
+            # if data is not clean, pass form back out
+            return render(request, "encyclopedia/add.html", {
+                "form": form
+            })
+    # otherwise if it's not a post return a blank form
+    return render(request, 'encyclopedia/add.html', {
+        "form": NewTitleForm()
+    })
